f1c4cf5829ecc8120e4151c5f44b622eecbeff34
ReggieGrant <grantreggie09@gmail.com>
2025-09-29 12:58:09 -0700

    class2

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from http import HTTPStatus
 
 app = Flask(__name__)
 
@@ -64,28 +65,117 @@ def add_student():
 
 
 # -------- Assignment #1 --------
-@app.route("/products", methods=["GET"])
-def get_products():
-    get_products = [
-        {"id": 1, "name": "Cake", "price": 25},
-        {"id": 2, "name": "Ice-cream", "price": 5},
-        {"id": 3, "name": "Cookie", "price": 3},
-        {"id": 4, "name": "Chocolate", "price": 10}
-    ]
-    return get_products
+products = [
+    {"id": 1, "name": "Cake", "price": 25},
+    {"id": 2, "name": "Ice-cream", "price": 5},
+    {"id": 3, "name": "Cookie", "price": 3},
+    {"id": 4, "name": "Chocolate", "price": 10}
+]
 
-@app.route("/products/count", methods=['GET'])
+@app.route("/api/products", methods=["GET"])
+def get_products():
+    return products
+
+@app.route("/api/products/count", methods=['GET'])
 def get_products_count():
-    products = [
-        {"id": 1, "name": "Cake", "price": 25},
-        {"id": 2, "name": "Ice-cream", "price": 5},
-        {"id": 3, "name": "Cookie", "price": 3},
-        {"id": 4, "name": "Chocolate", "price": 10}
-    ]
     return ({
         "success": True,
         "count": len(products)
     }), 200
+
+
+# --------------- Assingment #2 -----------
+
+products = [
+    {"id": 1, "name": "Cake", "price": 25},
+    {"id": 2, "name": "Ice-cream", "price": 5},
+    {"id": 3, "name": "Cookie", "price": 3},
+    {"id": 4, "name": "Chocolate", "price": 10}
+]
+
+@app.route("/api/products", methods=["POST"])
+def create_product():
+    new_product = request.get_json()
+    print(new_product)
+    products.append(new_product)
+    return jsonify(new_product), HTTPStatus.CREATED
+
+@app.route("/api/products/<int:id>", methods=["GET"])
+def get_products_by_id(id):
+    for product in products:
+        if product.get("id") == id:
+            return jsonify(product), HTTPStatus.OK
+        print(f"product: {product}")
+    return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
+
+@app.route("/api/products/<int:id>", methods=["DELETE"])
+def delete_product(id):
+    for index, product in enumerate(products):
+        if product.get("id") == id:
+            products.pop(index)
+            return jsonify({"message": "Product deleted successfully"}), HTTPStatus.NO_CONTENT
+    return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+coupons = [
+    {"_id": 1, "code": "WELCOME10", "discount": 10},
+    {"_id": 2, "code": "FALL25", "discount": 25},
+    {"_id": 3, "code": "VIP500", "discount": 50}
+
+]
+
+# READ all coupons
+@app.route("/api/coupons", methods=["GET"])
+def get_coupons():
+    return jsonify(coupons), HTTPStatus.OK
+
+
+# CREATE add a new coupon
+@app.route("/api/coupons", methods=["POST"])
+def create_coupon():
+    new_coupon = request.get_json()
+    print(new_coupon)
+    coupons.append(new_coupon)
+    return jsonify(new_coupon), HTTPStatus.CREATED
+
+
+
+# Path parameter
+# http://127.0.0.1:5000/greet/reggie
+@app.route("/greet/<string:name>", methods=["GET"])
+def greet(name):
+    return f"hello {name}", HTTPStatus.OK
+
+
+
+# GET a coupon 
+@app.route("/api/coupons/<int:id>", methods=["GET"])
+def get_coupon_by_id(id):
+    for coupon in coupons:
+        if coupon["_id"] == id:
+            return jsonify(coupon), HTTPStatus.OK
+        print(f"coupon: {coupon}")
+    return jsonify({"message": "Coupon not found"}), HTTPStatus.NOT_FOUND
+
+
+# UPDATE - 
+
+
+
+# DELETE - delete a coupon
+@app.route("/api/coupons/<int:id>", methods=["DELETE"])
+def delete_coupon(id):
+    for index, coupon in enumerate(coupons):
+        if coupon["_id"] == id:
+            coupons.pop(index)
+            return jsonify({"message": "Coupon deleted successfully"}), HTTPStatus.NO_CONTENT
+    return "testing"
     
 
 if __name__ == "__main__":
