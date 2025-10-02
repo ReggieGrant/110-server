ebd44e88b18d2b4d7f234584e6beaa2208ed98ff
ReggieGrant <grantreggie09@gmail.com>
2025-10-01 20:38:20 -0700

    class4

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from http import HTTPStatus
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # http://127.0.0.1:5000/
 @app.route("/", methods=["GET"])
@@ -65,11 +67,18 @@ def add_student():
 
 
 # -------- Assignment #1 --------
+# products = [
+#    {"id": 1, "name": "Cake", "price": 25},
+#    {"id": 2, "name": "Ice-cream", "price": 5},
+#    {"id": 3, "name": "Cookie", "price": 3},
+#    {"id": 4, "name": "Chocolate", "price": 10}
+#]
+
 products = [
-    {"id": 1, "name": "Cake", "price": 25},
-    {"id": 2, "name": "Ice-cream", "price": 5},
-    {"id": 3, "name": "Cookie", "price": 3},
-    {"id": 4, "name": "Chocolate", "price": 10}
+  {"id": 1, "title": "Cake", "price": 25, "category": "Electronics", "image": "https://picsum.photos/seed/1/300/300"},
+  {"id": 2, "title": "Ice-cream", "price": 5, "category": "Kitchen", "image": "https://picsum.photos/seed/2/300/300"},
+  {"id": 3, "title": "Cookie", "price": 3, "category": "Electronics", "image": "https://picsum.photos/seed/3/300/300"},
+  {"id": 4, "title": "Chocolate", "price": 10, "category": "Entertainment", "image": "https://picsum.photos/seed/4/300/300"}
 ]
 
 @app.route("/api/products", methods=["GET"])
@@ -86,12 +95,12 @@ def get_products_count():
 
 # --------------- Assingment #2 -----------
 
-products = [
-    {"id": 1, "name": "Cake", "price": 25},
-    {"id": 2, "name": "Ice-cream", "price": 5},
-    {"id": 3, "name": "Cookie", "price": 3},
-    {"id": 4, "name": "Chocolate", "price": 10}
-]
+# products = [
+#    {"id": 1, "name": "Cake", "price": 25},
+#    {"id": 2, "name": "Ice-cream", "price": 5},
+#    {"id": 3, "name": "Cookie", "price": 3},
+#    {"id": 4, "name": "Chocolate", "price": 10}
+#]
 
 @app.route("/api/products", methods=["POST"])
 def create_product():
@@ -117,8 +126,40 @@ def delete_product(id):
     return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
     
 
+# Only run the server once
 if __name__ == "__main__":
     app.run(debug=True)
+
+# UPDATE /api/products/2
+@app.route("/api/products/<int:id>", methods=["PUT"])
+def update_product(id):
+    data = request.get_json()
+    print(data)
+    for product in products:
+        if product["id"] == id:
+            product["name"] = data.get("name")
+            product["price"] = data.get("price")
+            return jsonify({"message": "Product updated successfully"}), HTTPStatus.OK
+    return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
+
+
+# --------- Session #4 ------------
+# Query paramters
+# A query parameter is added to the end of the URL to filter, sort of modify the response
+# GET /api/products/search?name=xxxx
+@app.route("/api/products/search", methods=["GET"])
+def get_product_by_name():
+    keyword = request.args.get("name").lower()
+    matched = []
+    for product in products:
+        if product["name"].lower() == keyword.lower():
+            matched.append(product)
+    print(keyword)
+    return jsonify({"results": matched}), HTTPStatus.OK
+        
+
+
+
 
 
 
@@ -147,6 +188,7 @@ def create_coupon():
 
 
 # Path parameter
+# A path parameter is a dynamic segment of the URL to pinpoint a specific item or resource.
 # http://127.0.0.1:5000/greet/reggie
 @app.route("/greet/<string:name>", methods=["GET"])
 def greet(name):
@@ -176,6 +218,27 @@ def delete_coupon(id):
             coupons.pop(index)
             return jsonify({"message": "Coupon deleted successfully"}), HTTPStatus.NO_CONTENT
     return "testing"
+
+# -------- Final report -------------
+
+@app.route("/api/coupons/<int:id>", methods=["PUT"])
+def update_coupon(id):
+    data = request.get_json()
+    print(data)
+    for coupon in coupons:
+        if coupon["_id"] == id:
+            coupon["code"] = data.get("code", coupon["code"])
+            coupon["discount"] = data.get("discount", coupon["discount"])
+            return jsonify({"message": "Coupon updated successfully", "coupon": coupon}), HTTPStatus.OK
+    return jsonify({"message": "Coupon not found"}), HTTPStatus.NOT_FOUND
+
+@app.route("/api/coupons/search", methods=["GET"])
+def search_coupons():
+    filtered_coupons = []
+    for coupon in coupons:
+        if coupon["discount"] < 30:
+            filtered_coupons.append(coupon)
+    return jsonify({"results": filtered_coupons, "count": len(filtered_coupons)}), HTTPStatus.OK
     
 
 if __name__ == "__main__":
